c407a495271d7258eac5aca794a5e4b5d56f6862
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2021-11-24 20:39:20 -0300

    Add CatalogRepository to Services DI Container

--- Services/Catalog/Catalog.API/Startup.cs
+++ Services/Catalog/Catalog.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.API.Infra;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Catalog.API.Infra.Repository;
 
 namespace DevShop.Services.ProductCatalog
 {
@@ -25,9 +28,10 @@ namespace DevShop.Services.ProductCatalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<EventCatalogDbContext>(options =>
-      options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<CatalogContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddScoped<ICatalogItemRepository, CatalogItemRepository>();
             services.AddControllers();
         }
 
