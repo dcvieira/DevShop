7034e9cc2b28548665beefc429036016827b6f8d
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2021-11-24 20:34:40 -0300

    Add GetCatalogItems Repository

---
+++ Services/Catalog/Catalog.API/Infra/Repository/ICatalogItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Infra.Repository
+{
+    public interface ICatalogItemRepository
+    {
+        Task<IEnumerable<CatalogItem>> GetCatalogItems();
+    }
+}
