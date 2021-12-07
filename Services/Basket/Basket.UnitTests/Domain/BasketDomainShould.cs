b31d85b33b56d230b7a7c0b54363f05eb1f44eb2
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2021-12-07 01:43:28 -0300

    Test basket domain

---
+++ Services/Basket/Basket.UnitTests/Domain/BasketDomainShould.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Basket.API.Domain;
+using Xunit;
+
+namespace Basket.UnitTests.Domain
+{
+    public class BasketDomainShould
+    {
+        public BasketDomainShould()
+        {
+        }
+
+
+        [Fact]
+        public void Add_basket_Item()
+        {
+            // Arrange
+            var basket = new API.Domain.Basket();
+            var basketDomain = new BasketDomain(basket);
+            var productId = Guid.NewGuid();
+            var productName = "test product";
+
+            // Act
+            basketDomain.AddBasketItem(productId, productName, 20, 1, "");
+
+            // Assert
+            Assert.Single(basketDomain.GetBasket().Items);
+            Assert.Equal(productId, basketDomain.GetBasket().Items[0].ProductId);
+            Assert.Equal(productName, basketDomain.GetBasket().Items[0].ProductName);
+
+        }
+
+        [Fact]
+        public void Increment_basket_Item_quantity_when_product_exist()
+        {
+            // Arrange
+            var basket = new API.Domain.Basket();
+            var basketDomain = new BasketDomain(basket);
+            var productId = Guid.NewGuid();
+            var productName = "test product";
+            basketDomain.AddBasketItem(productId, productName, 20, 1, "");
+
+            // Act
+            basketDomain.AddBasketItem(productId, productName, 20, 1, "");
+
+            // Assert
+            Assert.Single(basketDomain.GetBasket().Items);
+            Assert.Equal(productId, basketDomain.GetBasket().Items[0].ProductId);
+            Assert.Equal(2, basketDomain.GetBasket().Items[0].Quantity);
+
+        }
+
+        [Fact]
+        public void Update_basket_item_quantity()
+        {
+            // Arrange
+            var basket = new API.Domain.Basket();
+            var basketDomain = new BasketDomain(basket);
+            var productId = Guid.NewGuid();
+            var productName = "test product";
+            basketDomain.AddBasketItem(productId, productName, 20, 1, "");
+
+            // Act
+            basketDomain.UpdateBasketItemQuantity(productId, 3);
+
+            // Assert
+            Assert.Single(basketDomain.GetBasket().Items);
+            Assert.Equal(productId, basketDomain.GetBasket().Items[0].ProductId);
+            Assert.Equal(3, basketDomain.GetBasket().Items[0].Quantity);
+        }
+
+        [Fact]
+        public void Remove_basket_item()
+        {
+            // Arrange
+            var basket = new API.Domain.Basket();
+            var basketDomain = new BasketDomain(basket);
+            var productId = Guid.NewGuid();
+            var productName = "test product";
+            basketDomain.AddBasketItem(productId, productName, 20, 1, "");
+            var basketItemId = basketDomain.GetBasket().Items[0].Id;
+
+            // Act
+            basketDomain.RemoveBasketItem(basketItemId);
+
+            // Assert
+            Assert.Empty(basketDomain.GetBasket().Items);
+        }
+
+    }
+}
